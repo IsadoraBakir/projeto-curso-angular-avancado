bb18ad961554940ef43895a6c171552cd1ee12c1
Isadora Bakir Santana <ibakirsa@everis.com>
2021-01-30 13:43:45 -0300

    Include authentication module

---
+++ src/app/conta/conta.app.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+    // tslint:disable-next-line: component-selector
+    selector: 'conta-app-root',
+    template: '<router-outlet></router-outlet>'
+})
+export class ContaAppComponent { }
--- src/app/conta/conta.module.ts
+++ src/app/conta/conta.module.ts
@@ -1,14 +1,27 @@
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
 import { CommonModule } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+import { ContaRoutingModule } from './conta.route';
+
+import { ContaAppComponent } from './conta.app.component';
 import { CadastroComponent } from './cadastro/cadastro.component';
 import { LoginComponent } from './login/login.component';
 
-
-
 @NgModule({
-  declarations: [CadastroComponent, LoginComponent],
+  declarations: [
+    ContaAppComponent,
+    CadastroComponent,
+    LoginComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    ContaRoutingModule
   ]
 })
 export class ContaModule { }
